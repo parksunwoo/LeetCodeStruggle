ca7ef9fe7aba176e0f355074a723ed111eba19f4
parksunwoo <psw261@gmail.com>
2021-09-13 23:44:09 +0900

    add hash table

--- 11-hashtable/771-jewels-and-stones.py
+++ 11-hashtable/771-jewels-and-stones.py
@@ -4,25 +4,51 @@ import heapq
 
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freqs = {}
+        # 1
+        # freqs = {}
         
-        for char in stones:
-            if char not in freqs:
-                freqs[char] = 1
+        # for char in stones:
+        #     if char not in freqs:
+        #         freqs[char] = 1
                 
-            else:
-                freqs[char] += 1
+        #     else:
+        #         freqs[char] += 1
                 
-                
-        count = 0
-        for char in jewels:
-            if char in freqs:
-                count += freqs[char]
+        # count = 0
+        # for char in jewels:
+        #     if char in freqs:
+        #         count += freqs[char]
         
-        return count
+        # return count
+        
+        # 2
+        # freqs = collections.defaultdict(int)        
+        # count = 0
+        
+        # for char in stones:
+        #     freqs[char] += 1
+            
+        # for char in jewels:
+        #     count += freqs[char]
+            
+        # return count
+        
+        # 3
+        # freqs = collections.Counter(stones)
+        # count = 0
+        # for char in jewels:
+            # count += freqs[char]
+            
+        # return count
+        
+        # 4
+        return sum(s in jewels for s in stones)
+    
+        
+        
             
 if __name__ == '__main__':
     sol = Solution()
-    jewels = "z"
-    stones = "ZZ"
+    jewels = "aA"
+    stones = "aAAbbbb"
     print(sol.numJewelsInStones(jewels, stones))
