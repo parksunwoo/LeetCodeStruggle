b707a162ec9e55f29ba2782a2ac3849c620170e4
parksunwoo <psw261@gmail.com>
2021-09-27 00:30:59 +0900

    add dfs solution

---
+++ 12-graph/200-number-of-islands.py
@@ -0,0 +1,44 @@
+import collections
+from typing import Collection, Optional, List
+
+class Solution:
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        if i < 0 or i >= len(grid) or \
+            j < 0 or j >= len(grid[0]) or \
+                grid[i][j] != '1':
+                    return
+                
+        grid[i][j] = '0'
+        
+        self.dfs(grid, i+1 ,j)
+        self.dfs(grid, i-1 ,j)
+        self.dfs(grid, i ,j+1)
+        self.dfs(grid, i ,j-1)
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    
+                    count += 1
+        return count
+        
+        
+        
+        
+        
+            
+if __name__ == '__main__':
+    sol = Solution()
+    grid = [
+    ["1","1","1","1","0"],
+    ["1","1","0","1","0"],
+    ["1","1","0","0","0"],
+    ["0","0","0","0","0"]
+    ]
+    print(sol.numIslands(grid))
