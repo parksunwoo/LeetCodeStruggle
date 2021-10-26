ce6cc728e42f93462f7332796ce507c9c6d93fea
parksunwoo <psw261@gmail.com>
2021-10-27 07:54:51 +0900

    add depth of binary tree

---
+++ 13-graph2/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,35 @@
+import collections
+from typing import Collection, Optional, List
+import itertools
+import heapq
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        queue = collections.deque([root])
+        depth = 0
+
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        return depth
+
+if __name__ == '__main__':
+    sol = Solution()
+    root = [3,9,20, None, None, 15,7]
+    
+    print(sol.maxDepth(root))
