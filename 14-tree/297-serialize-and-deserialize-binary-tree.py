5730a37846f78d26ea30b13213f8a4c61590c71c
parksunwoo <psw261@gmail.com>
2021-11-08 07:07:19 +0900

    add serialize and deserialize tree

---
+++ 14-tree/297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,71 @@
+import collections
+from typing import Collection, Optional, List
+import itertools
+import heapq
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        
+        queue = collections.deque([(root)])
+        result = ['#']
+        
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+                result.append(str(node.val))            
+            else:
+                result.append('#')
+        return ' '.join(result)
+        
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        if data == '# #':
+            return None
+        
+        nodes = data.split()
+        
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+        index = 2
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index  += 1
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index  += 1
+        return root
+        
+# Your Codec object will be instantiated and called as such:
+if __name__ == '__main__':
+    root = [1,2,3,null,null,4,5]
+    ser = Codec()
+    deser = Codec()
+    ans = deser.deserialize(ser.serialize(root))
+    
+    
+    
