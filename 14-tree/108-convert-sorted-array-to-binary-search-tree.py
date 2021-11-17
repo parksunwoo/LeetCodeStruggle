4c3fea1608bdc7e2bf8dcebd68babb17010ee8aa
parksunwoo <psw261@gmail.com>
2021-11-17 23:13:31 +0900

    add sortedArrayToBST

---
+++ 14-tree/108-convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,41 @@
+import collections
+from typing import Collection, Optional, List
+import itertools
+import heapq
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1 :])
+        
+        return node
+    
+            
+                
+# Your Codec object will be instantiated and called as such:
+if __name__ == '__main__':
+    nums = [-10,-3,0,5,9]
+    sol = Solution()
+    print(sol.sortedArrayToBST(nums))
+    
+    
+    
+    
+    
+    
+    
+    
+    
