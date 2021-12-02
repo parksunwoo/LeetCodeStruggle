0a43a9ff9a2b63a03756a274896645aef6017868
parksunwoo <psw261@gmail.com>
2021-12-03 07:31:18 +0900

    add palindrome pairs

--- 15-heap/208-implement-trie-prefix-tree.py
+++ 15-heap/208-implement-trie-prefix-tree.py
@@ -42,22 +42,12 @@ class Trie:
             
         return True
 
-        
 
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)      
         
 # Your Codec object will be instantiated and called as such:
 if __name__ == '__main__':
-    # nums = [3,2,1,5,6,4]
-    # k = 2
-    
-    nums = [3,2,3,1,2,4,5,5,6]
-    k = 4
-    sol = Solution()
-    print(sol.findKthLargest(nums, k))
-    
+    obj = Trie()
+    obj.insert(word)
+    param_2 = obj.search(word)
+    param_3 = obj.startsWith(prefix)      
