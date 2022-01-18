26139cd124994876c6b29ecb4910520b2bf3eba0
parksunwoo <psw261@gmail.com>
2022-01-19 08:00:24 +0900

    add majority elem

--- 22-divide-and-conquer/169-majority-element.py
+++ 22-divide-and-conquer/169-majority-element.py
@@ -7,7 +7,37 @@ import bisect
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # sol 1
+        # for num in nums:
+        #     if nums.count(num) > len(nums) //2 :
+        #         return num
+
+        # sol 2
+        # counts = collections.defaultdict(int)
+        # for num in nums:
+            
+        #     if counts[num] == 0:
+        #         counts[num] = nums.count(num)
+                
+        #     if counts[num] > len(nums)//2:
+        #         return num
+            
+        # sol 3
+        # if not nums:
+        #     return None
         
+        # if len(nums) == 1:
+        #     return nums[0]
+        
+        # half = len(nums) // 2
+        # a = self.majorityElement(nums[:half])
+        # b = self.majorityElement(nums[half:])
+        
+        # return [b, a][nums.count(a) > half]
+
+        # sol 4
+        return sorted(nums)[len(nums) // 2]
+            
             
             
 # Your Codec object will be instantiated and called as such:
@@ -15,14 +45,5 @@ if __name__ == '__main__':
     nums = [3,2,3]
     sol = Solution()
     print(sol.majorityElement(nums))
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
---
+++ 22-divide-and-conquer/241-diff-ways-to-compute.py
@@ -0,0 +1,20 @@
+import collections
+from typing import Collection, Optional, List, Set
+import itertools
+import heapq
+import sys
+import bisect
+
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+
+
+
+
+            
+# Your Codec object will be instantiated and called as such:
+if __name__ == '__main__':
+    expression = "2-1-1"
+    sol = Solution()
+    print(sol.diffWaysToCompute(expression))
+    
