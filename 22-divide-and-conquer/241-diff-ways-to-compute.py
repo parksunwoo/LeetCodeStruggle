c4d162c6211304133d5db55f92d3de9ae6d664d1
parksunwoo <psw261@gmail.com>
2022-01-20 07:52:57 +0900

    add diff ways to compute

--- 22-divide-and-conquer/241-diff-ways-to-compute.py
+++ 22-divide-and-conquer/241-diff-ways-to-compute.py
@@ -1,4 +1,5 @@
 import collections
+from turtle import left, right
 from typing import Collection, Optional, List, Set
 import itertools
 import heapq
@@ -6,17 +7,30 @@ import sys
 import bisect
 
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-
-
-
-
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        def compute(left, right, op):
+            results = []
             
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+        
+        if input.isdigit():
+            return [int(input)]
+    
+        results = []
+        for index, value in enumerate(input):
+            if value in "-+*":
+                left = self.diffWaysToCompute(input[:index])
+                right = self.diffWaysToCompute(input[index+1:])
+                results.extend(compute(left, right, value))
+        return results
+
+
+
 # Your Codec object will be instantiated and called as such:
 if __name__ == '__main__':
     expression = "2-1-1"
     sol = Solution()
     print(sol.diffWaysToCompute(expression))
-
-
-    
