f6552298e37e10cf28cb2d76436b93460da2c313
parksunwoo <psw261@gmail.com>
2022-02-04 07:12:54 +0900

    add dart

--- 24-kakao/b2-dart.py
+++ 24-kakao/b2-dart.py
@@ -6,10 +6,32 @@ from typing import Collection, Optional, List, Set
 
 class Solution:
     def dart(self, dartResult:str) -> int:
-        return 0
+        nums = [0]
+        
+        for s in dartResult:
+            if s == 'S':
+                nums[-1] **= 1
+                nums.append(0)
+            elif s == 'D':
+                nums[-1] **= 2
+                nums.append(0)
+            elif s == 'T':
+                nums[-1] **= 3
+                nums.append(0)
+            elif s == '*':
+                nums[-2] *= 2
+                if len(nums) >= 2:
+                    nums[-3] *= 2                    
+            elif s == '#':
+                nums[-2] *= -1
+            else:
+                nums[-1] = nums[-1]*10 + int(s)
+        
+        return sum(nums)
             
 # Your Codec object will be instantiated and called as such:
 if __name__ == '__main__':
-    result = "1S2D*3T"
+    # result = "1S2D*3T"
+    result = "1D2S3T*"
     sol = Solution()
     print(sol.dart(result))
